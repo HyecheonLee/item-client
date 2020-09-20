1e99a1694e0b4027b00e666a3393c7e0bc36626b
hyecheon <rainbow880616@gmail.com>
2020-09-20 17:51:56 +0900

    explore the retrieve and exchange in webclient

--- src/main/kotlin/com/hyecheon/itemclient/controller/ItemClientController.kt
+++ src/main/kotlin/com/hyecheon/itemclient/controller/ItemClientController.kt
@@ -1,8 +1,31 @@
 package com.hyecheon.itemclient.controller
 
+import com.hyecheon.itemclient.domain.*
 import org.springframework.web.bind.annotation.*
+import org.springframework.web.reactive.function.client.*
+import reactor.core.publisher.*
 
 @RestController
 class ItemClientController {
+	val webClient: WebClient = WebClient.create("http://localhost:8080")
+
+	@GetMapping("/client/retrieve")
+	fun getAllItemsUsingRetrieve(): Flux<Item> {
+		return webClient.get().uri("/v1/items")
+				.retrieve()
+				.bodyToFlux(Item::class.java)
+				.log("Items in Client Project")
+	}
+
+	@GetMapping("/client/exchange")
+	fun getAllItemsUsingExchange(): Flux<Item> {
+		return webClient.get().uri("/v1/items")
+				.exchange()
+				.flatMapMany { clientResponse ->
+					clientResponse.bodyToFlux(Item::class.java)
+				}
+				.log("Items in Client Project exchange : ")
+	}
+
 
 }
